acda03153a18f6e86b886afc5468ec43603b8158
mrbrett <brett@cirrously.com>
2013-08-26 17:21:12 -0600

    #1 finished the random image generator plug. Booya. I shall write a wiki shortly that explains options and behaviour

--- js/main.js
+++ js/main.js
@@ -24,8 +24,9 @@
 			self.$navBtn.on("click", self.closetNav);
 
 			self.hideLoader();
-			self.heads();
+			// self.heads();
 			self.scrollPosition();
+			self.randomImages('.heads-container', 0, 0);
 			self.introCenter();
 			//self.workHover();
 		},
@@ -82,21 +83,104 @@
 			$intro.css('top', diff);			
 		},
 
-		heads: function() {
-			var wH = page.wH / 1.5,
-					wW = page.wW / 1.5,
-					$heads = $('.heads-container img');
+		randomImages: function(wrap, overlap, rotation) {
+			// set initial variables
+			var self = this,
+			$wrap = $(wrap),
+			$wrapImgs = $(wrap + ' img'),
+			wrapWidth = parseInt($wrap.css('width'))-overlap,
+			wrapHeight = parseInt($wrap.css('height'))-overlap,
+			imgPositionLeft = [],
+			imgPositionRight = [],
+			imgPositionTop = [],
+			imgPositionBottom = [];
 
-			$.each($heads, function() {
-				var randomW = Math.floor(Math.random()*wW);
-				var randomH= Math.floor(Math.random()*wH);
+			// loop through each img
+			$.each($wrapImgs, function() {
+				// Set variables specific to specific img or variables that need to be reset each
+				// new img
+				var imgWidth = parseInt($(this).css('width')),
+					  imgHeight = parseInt($(this).css('height')),
+					  imgTop = Math.random()*(wrapHeight-imgHeight)+overlap,
+						imgLeft = Math.random()*(wrapWidth-imgWidth)+overlap,
+						imgRight = imgLeft+imgWidth-overlap,
+						imgBottom = imgTop+imgHeight-overlap,
+						degRot = (rotation-(Math.random()*(rotation*2))) + 'deg',
+						attempt = 0;
 
+				// if overlap value given is greater than an overlap can be then set it to something
+				// workable
+				if(overlap > (imgWidth-10) || overlap > (imgHeight-10)) {
+					if(imgWidth < imgHeight){
+						overlap = imgWidth-10;
+					} else {
+						overlap = imgHeight-10;
+					}
+				}
+
+				// if setting position of first picture, skip checking for collision/overlap
+				if(imgPositionLeft.length === 0) {
+					$(this).css({
+						'top': imgTop + 'px',
+						'left': imgLeft + 'px'
+					});
+
+					// Push up first img positions to proper arrays for checking for collisions/overlap
+					imgPositionTop.push(imgTop);
+					imgPositionLeft.push(imgLeft);
+					imgPositionRight.push(imgRight);
+					imgPositionBottom.push(imgBottom);
+					
+				} else {
+					// For loop for checking overlap/collisions with all already placed imgs
+					for(var i=0; i<imgPositionTop.length; i++) {
+						// Logic for overlap/collision: must pass both logic statements for actual overlap
+						if((imgLeft>imgPositionLeft[i] && imgLeft<imgPositionRight[i]) || (imgRight>imgPositionLeft[i] && imgRight<imgPositionRight[i])) {
+							if((imgTop>imgPositionTop[i] && imgTop<imgPositionBottom[i]) || (imgBottom>imgPositionTop[i] && imgBottom<imgPositionBottom[i])) {
+								attempt++;
+								// If 3000 placement attempts are made then ignore overlap rules and place
+								// picture. This only happens when browser is small (chances exponentially
+								// grow small the window) and is necessary for small browser windows escaping
+								// infinite loop
+								if(attempt>3000) {
+									imgTop = Math.random()*(wrapHeight-imgHeight);
+									imgLeft = Math.random()*(wrapWidth-imgWidth);
+									imgRight = imgLeft+imgWidth;
+									imgBottom = imgTop+imgHeight;
+								} else {
+									// Choose another random img placement since failed overlap logic and
+									// set iteration back to the beginning of the loop to check all existing
+									// imgs that are placed
+									imgTop = Math.random()*(wrapHeight-imgHeight)+overlap,
+									imgLeft = Math.random()*(wrapWidth-imgWidth)+overlap,
+									imgRight = imgLeft+imgWidth-overlap,
+									imgBottom = imgTop+imgHeight-overlap;
+									i=-1;
+								}
+							}
+						}
+					}
+					// After finding a successful img placement push positions to arrays
+					imgPositionTop.push('top', imgTop);
+					imgPositionLeft.push('left', imgLeft);
+					imgPositionRight.push('right',imgRight);
+					imgPositionBottom.push('bottom',imgBottom);
+					// After finding a successful img placement place the img
+					$(this).css({
+						'top': imgTop + 'px',
+						'left': imgLeft + 'px'
+					});
+				}
+				// Set rotation
 				$(this).css({
-					top: randomH,
-					left: randomW
-				});
+					'-moz-transform':'rotate('+degRot+')',
+					'-webkit-transform':'rotate('+degRot+')',
+					'-o-transform':'rotate('+degRot+')',
+					'-ms-transform':'rotate('+degRot+')',
+					'transform':'rotate('+degRot+')'
+				})
+				
 			})
-			
 		},
 
 		workHover: function() {
